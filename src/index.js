4d5907d0573315a6b163e837e2fa7c11d0fad1b2
MaximoEspitia <max19espitia@gmail.com>
2022-03-25 20:33:07 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let textFun = document.querySelector('form')
-  textFun.addEventListener('sumbit', (e) => {
+  let textFun = document.getElementById('create-task-form')
+  textFun.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+  let description = document.getElementById('new-task-description') 
+    const newTask = document.createElement("li")
+    newTask.innerText = description.value
+  let toDoContainer = document.getElementById("tasks")
+  toDoContainer.appendChild(newTask)
+  e.target.reset()
+  //console.log(e)
   })
 })
